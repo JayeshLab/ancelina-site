ad72e34d0e4339154239f7cb44b899f3ad51ea5d
Jayesh Vachhani <helloiamjayesh@gmail.com>
2024-08-13 17:11:53 +0530

    removed not used

--- formulieren/fcf-assets/900707.process.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ************************************
-// This file is part of a package from:
-// www.majesticform.com
-// See license for details
-// ************************************
-
-require dirname(__FILE__).'/900707.config.php';
-require dirname(__FILE__).'/process.php';
