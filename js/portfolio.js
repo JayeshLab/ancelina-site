c3051b35dddc2cd861fd3cc7b6f97a42508c855d
Jayesh Vachhani <helloiamjayesh@gmail.com>
2024-08-14 16:35:56 +0530

    Test without once: true

---
+++ js/portfolio.js
@@ -0,0 +1,5 @@
+function doSomething() {
+  console.log("doSomething is called in portfolio.js");
+}
+console.log("PORTFOLIO function called");
+window.addEventListener("DOMContentLoaded", doSomething);
