b66a8f5e6b0d1d26cc262cc399ad2a5fa0bc76ff
Jayesh Vachhani <helloiamjayesh@gmail.com>
2024-09-16 15:53:25 +0530

    added ajaxify-init js

---
+++ js/ajaxify-init.js
@@ -0,0 +1,12 @@
+let myAjaxify = new Ajaxify({
+  elements: "#main-content, #navbarCollapse, #footer-stay",
+  verbosity: 2,
+  prefetchoff: !0,
+  scrolltop: !0,
+  pluginon: !0,
+  requestDelay: 0,
+  intevents: !0,
+  inlineskip: "storedTheme",
+  DCLDelay: 60,
+  alwayshints: "pgia.js, fcf.just-validate.min.js, swiper-bundle.min.js, cleave.min.js, smooth-scroll.polyfills.min.js, lazysizes.js, functions.js, index.js",
+});
